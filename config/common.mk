25b8112918c85b84daae751818f8daa52bdc80d1
LuK1337 <priv.luk@gmail.com>
2019-10-24 02:33:34 +0200

    vendor: Bring back Android Beam
    * While this feature is deprecated all code
      for it is still alive ^.^
    
    Change-Id: I36fac374e43ceddab1438255654bb541937041ff
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -73,6 +73,10 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-aosip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-aosip.xml \
     vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml
 
+# Enable Android Beam on all targets
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
